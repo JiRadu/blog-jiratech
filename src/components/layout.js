b191785c62264acedb27755a09780a274d21462e
Radu Jinga <jinga.radu@gmail.com>
2019-06-04 16:14:02 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,10 +4,7 @@ import PropTypes from "prop-types"
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <>
-  <h3>Testing blog reaction</h3>
   <main>{children}</main>
-  </>
 )
 
 Layout.propTypes = {
