f6925502cdeea761afe2ca09c200d23c594bc22a
Radu Jinga <jinga.radu@gmail.com>
2019-06-04 16:04:51 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,10 @@ import PropTypes from "prop-types"
 import "./layout.css"
 
 const Layout = ({ children }) => (
+  <>
+  <h3>Jiratech</h3>
   <main>{children}</main>
+  </>
 )
 
 Layout.propTypes = {
